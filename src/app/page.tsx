adf0a9d2fa6d51c4e259178de1bc0638eccb27d6
Prakash meena <meenaprak12@gmail.com>
2024-09-20 01:01:37 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 import { useEffect } from "react";
 
 const Spline = dynamic(() => import('@splinetool/react-spline'), {
   ssr: false,
-  loading: () => <p>...</p>,
+  loading: () =><div>
+    <Image src={'/FallbackImage.webp'} loading='lazy' unoptimized alt="photo" width="0" height="0" sizes="100vw" className='h-screen w-screen'/>
+  </div>,
 });
 
 const About = dynamic(() => import("@/Components/About"));
